aa28213d29e3fece13f0f4778dac143898e35f47
Akhil Peter George <Akhil16pro@gmail.com>
2023-08-30 17:03:10 +0400

    code

---
+++ js/title.js
@@ -0,0 +1,25 @@
+const title = document.querySelector('h1')
+
+const links = document.querySelectorAll('section a')
+
+const body = document.querySelector('body')
+const color = ['#2a9d8f', '#5A0001', '#415a77', '#606c38', '#173753', '#F45B69']
+links.forEach((link) => {
+  link.addEventListener('mouseenter', () => {
+    title.innerText = link.getAttribute('alt')
+    const rI = Math.floor(Math.random() * 7)
+    console.log(color[rI])
+    body.classList.add('hovered')
+    link.classList.add('hovered')
+
+    body.style.setProperty('--text-color', color[rI])
+  })
+
+  link.addEventListener('mouseleave', () => {
+    title.innerText = 'Grid Pan'
+    body.classList.remove('hovered')
+    link.classList.remove('hovered')
+
+    body.removeAttribute('style')
+  })
+})
